f1a965e03600cef7386564f54214bc26fe25cc3b
Muhammad Rafi <96472230+Ryurex-Code@users.noreply.github.com>
2023-08-28 21:00:22 +0800

    Create TestPython.py
    add a first code

---
+++ TestPython.py
@@ -0,0 +1,12 @@
+#Python Test
+print("Hello World")
+name = "Muhammad Rafi"
+age = 17
+print("My name is "+ name +", I am " + age + " years old."
+n = 1
+if n >= 2:
+  print ("Halo Ryurex")
+elif n >= 3:
+  print ("Helo F......i")
+else:
+  print("turu")
